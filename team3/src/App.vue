7549050aa7c4d93e3c2786f50f6b6167245bc50f
ismail ahmed <ismail.ahmed@hva.nl>
2022-10-07 13:23:42 +0200

    fix template

--- team3/src/App.vue
+++ team3/src/App.vue
@@ -1,31 +1,18 @@
 <template>
-  <Header/>
     <Welcome/>
-  <Footer/>
 </template>
 
 <script>
-import Header from'./components/Header.vue'
-import Footer from './components/Footer.vue'
 import Welcome from './components/Welcome.vue'
 
 export default {
   name: 'App',
   components: {
-    Header,
     Welcome,
-    Footer,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- team3/src/main.js
+++ team3/src/main.js
@@ -1,8 +1,14 @@
+import 'bootstrap'
+import './assets/css/main.css'
+import './assets/js/scripts'
+
 import { createApp } from 'vue'
+
 
 import App from './App.vue'
 //
 
 createApp(App).mount('#app')
-import 'bootstrap/dist/css/bootstrap.css'
+
+
 
