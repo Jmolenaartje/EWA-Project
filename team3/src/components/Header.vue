50d4baffa92f252d7c201208005c5147c43b5749
Jelmer <jelmer.van.rooijen@hva.nl>
2022-10-08 14:06:27 +0200

    profilePage

--- team3/src/components/Header.vue
+++ team3/src/components/Header.vue
@@ -1,4 +1,5 @@
 <template>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
             <div class="container">
                 <a class="navbar-brand" href="#page-top">Start Bootstrap</a>
--- team3/src/components/profilePage.vue
+++ team3/src/components/profilePage.vue
@@ -1,31 +1,118 @@
 <template>
-  <h1>Profile</h1>
-    <div class="container">
-        <div class="row">
-            <div class="col-4">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="card-link">Card link</a>
-                        <a href="#" class="card-link">Another link</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-8">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="card-link">Card link</a>
-                        <a href="#" class="card-link">Another link</a>
-                    </div>
-                </div>
-            </div>
-        </div>
+  <header class="masthead bg-primary text-white text-center">
+  <div class="container d-flex align-items-center flex-column">
+    <div class="row">
+      <div class="col-12">
+        <h1 class="text-center">Profile</h1>
+      </div>
     </div>
+    <div class="row">
+      <div class="col-12">
+        <div class="card">
+          <div class="card-body">
+            <div class="row">
+              <div class="col-12 col-md-4">
+                <img
+                  src="https://www.w3schools.com/howto/img_avatar.png"
+                  alt="Avatar"
+                  class="avatar"
+                />
+              </div>
+              <div class="col-12 col-md-8">
+                <div class="row">
+                  <div class="col-12">
+                    <h2>John Doe</h2>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-12">
+                    <p>
+                      <i class="fas fa-map-marker-alt"></i>
+                      Amsterdam, The Netherlands
+                    </p>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-12">
+                    <p>
+                      <i class="fas fa-envelope"></i>
+                      <a href="mailto:test@gmail.com">
+                        fkajldsjfkl@gmail.com
+                      </a>
+                    </p>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-12">
+                    <p>
+                      <i class="fas fa-phone"></i>
+                      <a href="tel:6234929">
+                        6234892349
+                      </a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Create a change password button -->
+    <div class="row">
+      <div class="col-12">
+        <button class="btn btn-primary">Change Password</button>
+        <button class="btn btn-primary">Change Email</button>
+      </div>
+    </div>
+  </div>
+
+    <!-- Create another card allign to the right only with h1-->
+    <div class="row">
+      <div class="col-12">
+        <div class="card">
+          <div class="card-body">
+            <h1>Level overview</h1>
+          </div>
+        </div>
+
+        <!-- Create a table with the following columns:level id, level name, level score -->
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Level ID</th>
+              <th scope="col">Level Name</th>
+              <th scope="col">Level Score</th>
+              <th scope="col">Level Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Level 1</td>
+              <td>100</td>
+              <td>Completed</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Level 2</td>
+              <td>200</td>
+              <td>Completed</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Level 3</td>
+              <td>300</td>
+              <td>Completed</td>
+            </tr>
+          </tbody>
+        </table>
+
+      </div>
+    </div>
+
+
+</header>
 </template>
 
 <script>
@@ -35,5 +122,63 @@ export default {
 </script>
 
 <style scoped>
+.avatar {
+  width: 100%;
+  border-radius: 50%;
+}
+
+.card {
+  margin-top: 2em;
+  margin-bottom: 2em;
+  border-radius: 0;
+  background-color: transparent;
+}
+
+.card-body {
+  padding: 2em;
+}
+
+h2 {
+  margin-top: 0;
+}
+
+p {
+  margin-bottom: 0;
+}
+
+a {
+  color: #4b68b8;
+}
+
+a:hover {
+  color: #4b68b8;
+  text-decoration: none;
+}
+.card-body {
+  padding: 2em;
+}
+
+.rowRight {
+  margin-left: 2em;
+}
+
+@media (min-width: 768px) {
+  .card {
+    border-radius: 0.25rem;
+  }
+}
+
+@media (min-width: 992px) {
+  .card {
+    border-radius: 0.25rem;
+  }
+}
+
+@media (min-width: 1200px) {
+  .card {
+    border-radius: 0.25rem;
+  }
+}
+
 
 </style>
