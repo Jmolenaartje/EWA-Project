5f7353442a28dd3b75bd9db5853ab11924fd7011
ismail ahmed <ismail.ahmed@hva.nl>
2022-10-09 01:18:14 +0200

    start page some html and css

--- team3/src/components/Header.vue
+++ team3/src/components/Header.vue
@@ -1,25 +1,36 @@
 <template>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-        <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
-            <div class="container">
-              <a class="navbar-brand"  ref="#page-top" ><router-link to="/home">
-                <img id="logo" src="../assets/img/Gemeente-Amsterdam-logo-groot-1.png"></router-link></a>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"/>
+  <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand" ref="#page-top">
+        <router-link to="/home">
+          <img id="logo" src="../assets/img/Gemeente-Amsterdam-logo-groot-1.png"></router-link>
+      </a>
 
-                <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    Menu
-                    <i class="fas fa-bars"></i>
-                </button>
+      <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button"
+              data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive"
+              aria-expanded="false" aria-label="Toggle navigation">
+        Menu
+        <i class="fas fa-bars"></i>
+      </button>
 
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded"><router-link to="/level-overview">Level overview </router-link></a></li>
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#about">About</a></li>
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#contact">Contact</a></li>
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded"><router-link to="/profilePage">My Profile </router-link></a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded">
+            <router-link to="/level-overview">Level overview</router-link>
+          </a></li>
+          <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#about">About</a></li>
+          <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#contact">Contact</a>
+          </li>
+          <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded">
+            <router-link to="/profilePage">My Profile</router-link>
+          </a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
@@ -29,6 +40,24 @@ export default {
 </script>
 
 <style>
-#logo{ height: 100px}
-a { text-decoration: none; }
+#mainNav {
+  background-color: white !important;
+
+  text-decoration-color: red;
+}
+
+
+
+#logo:hover, #logo {
+  height: 100px;
+}
+
+a {
+  text-decoration: none;
+  color: red;
+
+}
+
+
+
 </style>
--- team3/src/views/levelOverview.vue
+++ team3/src/views/levelOverview.vue
@@ -1,19 +1,59 @@
 <template>
-<br>
-<br>
-<br>
-<br>
-<br>
- 
+  <header class="masthead bg-primary text-white text-center">
+    <div class="container">
+      <div>
+        <div class="row-level" id="level-1">
+          <div class="level" @click="clickEvent"> level1</div>
+        </div>
+        <div class="row-level">
+          <div class="level"> level2</div>
 
+        </div>
+        <div class="row-level">
+          <div class="level"> level3</div>
+        </div>
+        <div class="row-level">
+          <div class="level"> level4</div>
+        </div>
+        <div class="row-level">
+          <div class="level"> level5</div>
+        </div>
+        <div class="row-level">
+          <div class="level"> level6</div>
+        </div>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
+
 export default {
-  name: "levelOverview"
+  name: "levelOverview",
+  methods: {
+    clickEvent() {
+      this.$router.push("/home");
+    }
+  }
 }
+
 </script>
 
+
 <style scoped>
+.row-level {
+  min-height: 100px;
+  padding: 20px;
+  position: relative;
+}
+
+.level {
+  padding: 30px;
+  max-height: 100px;
+  background-color: #3d3d3d;
+  float: bottom;
+  border-radius: 10px;
+}
+
 
 </style>
