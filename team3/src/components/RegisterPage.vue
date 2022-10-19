578ebedb9c9cfdae71a23bafd9113e4d150131ea
vinksfh <vink2002@hotmail.nl>
2022-10-19 11:33:32 +0200

    verificatie

--- team3/src/components/RegisterPage.vue
+++ team3/src/components/RegisterPage.vue
@@ -21,29 +21,36 @@
                   <input
                     type="email"
                     class="form-control"
-                    id="emailInput"
+                    v-model="email"
                     aria-describedby="emailHelp"
                     placeholder="Enter email"
                   />
-                  <label for="exampleInputEmail1">phone number</label>
+                  <label for="exampleInputEmail1">First name</label>
                   <input
-                    type="tel"
+                    type="text"
                     class="form-control"
-                    id="telInput"
+                    v-model="firstName"
+                    placeholder="phonenumber"
+                  />
+                  <label for="exampleInputEmail1">Last name</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    v-model="lastName"
                     placeholder="phonenumber"
                   />
                   <label for="exampleInputEmail1">Password</label>
                   <input
                     type="password"
                     class="form-control"
-                    id="passwordInput"
+                    v-model="password1"
                     placeholder="password"
                   />
                   <label for="exampleInputEmail1">Password</label>
                   <input
                     type="password"
                     class="form-control"
-                    id="password-check"
+                    v-model="password2"
                     placeholder="password"
                   />
                 </div>
@@ -69,9 +76,40 @@
 </template>
 
 <script>
-// export default {
-//   data: {}
-// };
+export default {
+  data() {
+    return{
+    error: [],
+    email: null,
+    phone: null,
+    password1: null,
+    password2: null,
+    }
+  },
+  methods: {
+       validate() {
+      this.errors = [];
+      if (!this.firstName) this.errors.push("First name is required");
+      if (!this.lastName) this.errors.push("Surname is required");
+      if (!this.email) this.errors.push("Email is required");
+      if (!this.password1) this.errors.push("Password is required");
+      if (!this.password2)
+        this.errors.push("Please repeat your password");
+      if (this.password1 != this.password2)
+        this.errors.push("The repeated password is not a match");
+    },
+     signup() {
+      this.validate();
+      if (this.errors.length !== 0) return;
+      // const user = {
+      //   firstName: this.first_name,
+      //   lastName: this.surname,
+      //   email: this.email,
+      //   password: this.password,
+      // };
+    },
+  }
+}
 </script>
 
 <style></style>
