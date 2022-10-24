82643297123b81dd390138524304bb2eab8c97b2
ismail ahmed <ismail.ahmed@hva.nl>
2022-10-24 23:17:55 +0200

    level1

--- team3/src/views/levelViews/levelOne.vue
+++ team3/src/views/levelViews/levelOne.vue
@@ -3,16 +3,60 @@
   <div class="masthead bg-primary text-white text-center" id="divBox">
     <div class="container">
       <div class="row">
-        <div class="col counterBox">s</div>
-        <div class="col counterBox">s</div>
-        <div class="col counterBox">s</div>
+
+        <div class="col counterBox">
+          <div class="row">
+            <div class="col" id="links"> <P> min </P></div>
+            <P class="col"> blind <a> counter</a> </P>
+            <div class="col" id="rechts"><p> plus </p></div>
+          </div>
+        </div>
+
+        <div class="col counterBox">
+          <div class="row">
+            <div class="col" id="links"> <P> min </P></div>
+            <P class="col"> blind</P>
+            <div class="col" id="rechts"><p> plus </p></div>
+          </div>
+        </div>
+
+
+        <div class="col counterBox">
+          <div class="row">
+            <div class="col" id="links"> <P> min </P></div>
+            <P class="col"> blind</P>
+            <div class="col" id="rechts"><p> plus </p></div>
+          </div>
+        </div>
+
       </div>
       <div class="row">
-        <div class="col counterBox">s</div>
-        <div class="col counterBox">s</div>
+
         <div class="col counterBox">
-          <div>jjj</div>
+          <div class="row">
+            <div class="col" id="links"> <P> min </P></div>
+            <P class="col"> blind <a> counter</a> </P>
+            <div class="col" id="rechts"><p> plus </p></div>
+          </div>
         </div>
+
+        <div class="col counterBox">
+          <div class="row">
+            <div class="col" id="links"> <P> min </P></div>
+            <P class="col"> blind</P>
+            <div class="col" id="rechts"><p> plus </p></div>
+          </div>
+        </div>
+
+
+        <div class="col counterBox">
+          <div class="row">
+            <div class="col" id="links"> <P> min </P></div>
+            <P class="col"> blind</P>
+            <div class="col" id="rechts"><p> plus </p></div>
+          </div>
+        </div>
+
       </div>
     </div>
   </div>
@@ -20,27 +64,49 @@
 
 <script>
 export default {
-  name: "levelOne"
+  name: "levelOne",
+
 }
 </script>
 
 <style scoped>
 .counterBox {
-  padding: 20px;
+
   margin: 10px;
   border-style: solid;
   height: 100px;
-  width: 300px;
-  border-radius: 150px;
+  width: 100px;
   background-color: red;
+  border-radius: 150px;
 }
 
-/*.box{*/
-/*  border-style: solid;*/
-/*  border-color: white !important;*/
-/*  max-height: 79px;*/
-/*  height: 78px;*/
-/*}*/
+#links {
+  border-style: solid;
+  background-color: yellow;
+  height: 98px;
+
+  max-width: 100px;
+  border-radius: 150px 0 0 150px;
+
+}
+
+p {
+  /*margin: 20px;*/
+  text-align: center;
+  color: #ffffff;
+  font-weight: bold;
+  font-size: 40px;
+}
+
+#rechts {
+  border-style: solid;
+  background-color: black;
+  text-align: center;
+  height: 98px;
+  max-width: 100px;
+  border-radius: 0 150px 150px 0;
+
+}
 
 
 </style>
