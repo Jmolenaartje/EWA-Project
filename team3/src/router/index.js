35eddfa171d0b5b49baa5d799a973099b1f1d6d3
vinksfh <vink2002@hotmail.nl>
2022-10-25 14:09:19 +0200

    index gefixed

--- team3/src/router/index.js
+++ team3/src/router/index.js
@@ -1,29 +1,20 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
-<<<<<<< HEAD
+
 import Welcome from '@/components/Welcome.vue'
 import profilePage from "@/views/profilePage.vue";
 import levelOverview from "@/views/levelOverview";
-
-const routes = [
-    { path: "/home", component: Welcome },
-    { path: "/profilePage", component: profilePage},
-    { path: "/level-overview", component: levelOverview},
-=======
-import Welcome from '@/components/Welcome.vue';
-import profilePage from "@/components/profilePage.vue";
 import SignIn from "../components/SignIn.vue";
 import RegisterPage from "../components/RegisterPage.vue";
 
-
-
 const routes = [
     { path: "/home", component: Welcome },
->>>>>>> Santi-login
     { path: "/", redirect: "/home"},
     { path: "/profilePage", component: profilePage},
     { path: "/signIn", component: SignIn},
-    { path: "/registerPage", component: RegisterPage}
+    { path: "/registerPage", component: RegisterPage},
+    { path: "/profilePage", component: profilePage},
+    { path: "/level-overview", component: levelOverview},
    ];
 
 export const router = createRouter({
