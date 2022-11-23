e0900b32f35491a0424068cae82c43e09c236657
vinksfh <vink2002@hotmail.nl>
2022-11-23 01:56:28 +0100

    model af :)

--- team3/src/views/levelViews/levelOne.vue
+++ team3/src/views/levelViews/levelOne.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="masthead bg-primary text-white text-center" id="divBox">
     <div id="ten-countdown"></div>
+    <input type="checkbox" class="rounded" v-model="checked" id="checkbox" />
+    <span  class="h1  ">ov</span>
 
     <div class="container">
-    
       <div class="row justify-content-center mx-2">
-        <div class="col-lg-3 m-1 counterBox border border-danger rounded bg-danger ">
+        <div
+          class="counterBox col-lg-3 m-1 counterBox rounded"
+          v-bind:class="checked ? 'background-checked' : 'bg-danger'"
+        >
           <div class="row">
             <div class="col p-1 display-5" id="links">
               <P @click="countMinus"> - </P>
             </div>
             <p class="col">
-              Blindheid<a
+              handicap1<a
                 ><br />
                 {{ total1 }}</a
               >
@@ -21,118 +25,143 @@
             </div>
           </div>
         </div>
-        
-         <div class="col-lg-3 m-1 counterBox border border-danger rounded bg-danger ">
+
+        <div
+          class="counterBox col-lg-3 m-1 counterBox rounded"
+          v-bind:class="checked ? 'background-checked' : 'bg-danger'"
+        >
           <div class="row">
             <div class="col p-1 display-5" id="links">
-              <P @click="countMinus"> - </P>
+              <P @click="countMinus2"> - </P>
             </div>
             <p class="col">
               Blindheid<a
                 ><br />
-                {{ total1 }}</a
+                {{ total2 }}</a
               >
             </p>
             <div class="col p-1 display-5" id="rechts">
-              <p @click="countPlus">+</p>
+              <p @click="countPlus2">+</p>
             </div>
           </div>
         </div>
-         <div class="col-lg-3 m-1 counterBox border border-danger rounded bg-danger ">
+        <div
+          class="counterBox col-lg-3 m-1 counterBox rounded"
+          v-bind:class="checked ? 'background-checked' : 'bg-danger'"
+        >
           <div class="row">
             <div class="col p-1 display-5" id="links">
-              <P @click="countMinus"> - </P>
+              <P @click="countMinus3"> - </P>
             </div>
             <p class="col">
               Blindheid<a
                 ><br />
-                {{ total1 }}</a
+                {{ total3 }}</a
               >
             </p>
             <div class="col p-1 display-5" id="rechts">
-              <p @click="countPlus">+</p>
+              <p @click="countPlus3">+</p>
             </div>
           </div>
         </div>
-        
       </div>
-          <div class="row justify-content-center mx-2">
-        <div class="col-lg-3 m-1 counterBox border border-danger rounded bg-danger ">
+      <div class="row justify-content-center mx-2">
+        <div
+          class="counterBox col-lg-3 m-1 counterBox rounded"
+          v-bind:class="checked ? 'background-checked' : 'bg-danger'"
+        >
           <div class="row">
             <div class="col p-1 display-5" id="links">
-              <P @click="countMinus"> - </P>
+              <P @click="countMinus4"> - </P>
             </div>
             <p class="col">
               Blindheid<a
                 ><br />
-                {{ total1 }}</a
+                {{ total4 }}</a
               >
             </p>
             <div class="col p-1 display-5" id="rechts">
-              <p @click="countPlus">+</p>
+              <p @click="countPlus4">+</p>
             </div>
           </div>
         </div>
-        
-         <div class="col-lg-3 m-1 counterBox border border-danger rounded bg-danger ">
+
+        <div
+          class="counterBox col-lg-3 m-1 counterBox rounded"
+          v-bind:class="checked ? 'background-checked' : 'bg-danger'"
+        >
           <div class="row">
             <div class="col p-1 display-5" id="links">
-              <P @click="countMinus"> - </P>
+              <P @click="countMinus5"> - </P>
             </div>
             <p class="col">
               Blindheid<a
                 ><br />
-                {{ total1 }}</a
+                {{ total5 }}</a
               >
             </p>
             <div class="col p-1 display-5" id="rechts">
-              <p @click="countPlus">+</p>
+              <p @click="countPlus5">+</p>
             </div>
           </div>
         </div>
-         <div class="col-lg-3 m-1 counterBox border border-danger rounded bg-danger ">
+        <div
+          class="counterBox col-lg-3 m-1 counterBox rounded"
+          v-bind:class="checked ? 'background-checked' : 'bg-danger'"
+        >
           <div class="row">
             <div class="col p-1 display-5" id="links">
-              <P @click="countMinus"> - </P>
+              <P @click="countMinus5"> - </P>
             </div>
             <p class="col">
               Blindheid<a
                 ><br />
-                {{ total1 }}</a
+                {{ total5 }}</a
               >
             </p>
             <div class="col p-1 display-5" id="rechts">
-              <p @click="countPlus">+</p>
+              <p @click="countPlus5">+</p>
             </div>
           </div>
         </div>
-        
       </div>
-      <button
-        class="btn btn-primary"
-        id="btn"
-        @click="countdown('ten-countdown', 30, 0)"
-      >
-        start
-      </button>
+      <div class="buttons">
+        <button
+          class="btn m-1 border btn-primary"
+          id="btn"
+          @click="countdown('ten-countdown', 5, 0)"
+        >
+          start
+        </button>
+          <button class="btn m-1 border btn-primary" @click="showModal = true">Save</button>
+         <SavedModal v-show="showModal" />
+
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import SavedModal from "../../components/Level2Modal.vue";
+
 export default {
+ 
   name: "levelOne",
 
   data() {
     return {
       total1: 0,
-      total2: null,
-      total3: null,
-      total4: null,
-      total5: null,
-      total6: null,
-      stopTimer: null,
+      total2: 0,
+      total3: 0,
+      total4: 0,
+      total5: 0,
+      totalscore: 0,
+      checked: false,
+      showModal: false,
     };
+  },
+  components: {
+    SavedModal,
   },
   methods: {
     countdown(elementName, minutes, seconds) {
@@ -166,7 +195,6 @@ export default {
       endTime = +new Date() + 1000 * (60 * minutes + seconds) + 500;
       updateTimer();
     },
-
     countPlus() {
       if (this.stopTimer) {
         this.total1++;
@@ -175,6 +203,21 @@ export default {
     countPlus2() {
       if (this.stopTimer) {
         this.total2++;
+      }
+    },
+    countPlus3() {
+      if (this.stopTimer) {
+        this.total3++;
+      }
+    },
+    countPlus4() {
+      if (this.stopTimer) {
+        this.total4++;
+      }
+    },
+    countPlus5() {
+      if (this.stopTimer) {
+        this.total5++;
       }
     },
     countMinus() {
@@ -186,8 +229,29 @@ export default {
     },
     countMinus2() {
       if (this.stopTimer) {
-        if (this.total1 > 0) {
+        if (this.total2 > 0) {
           this.total2--;
+        }
+      }
+    },
+    countMinus3() {
+      if (this.stopTimer) {
+        if (this.total3 > 0) {
+          this.total3--;
+        }
+      }
+    },
+    countMinus4() {
+      if (this.stopTimer) {
+        if (this.total4 > 0) {
+          this.total4--;
+        }
+      }
+    },
+    countMinus5() {
+      if (this.stopTimer) {
+        if (this.total5 > 0) {
+          this.total5--;
         }
       }
     },
@@ -196,6 +260,12 @@ export default {
 </script>
 
 <style scoped>
+a {
+  padding-bottom: 20px;
+}
+.background-checked {
+  background-color: rgb(91, 91, 167);
+}
 #ten-countdown {
   display: inline;
   padding: 5px;
@@ -203,8 +273,5 @@ export default {
   font-size: 4em;
   font-weight: bold;
   text-decoration: none;
-}
-a {
-  padding-bottom: 20px;
 }
 </style>
